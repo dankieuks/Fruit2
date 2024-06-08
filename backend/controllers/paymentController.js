b620fa3e8e746ae76a97fbd0cece46ef9a406cc6
dankieu <kieudinhdan02@gmail.com>
2024-06-09 01:15:52 +0700

    zlpay

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -2,7 +2,8 @@ const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const axios = require("axios");
 const crypto = require("crypto");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
+const moment = require("moment");
+const CryptoJS = require("crypto-js");
 // Process stripe payments   =>   /api/v1/payment/process
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
   const paymentIntent = await stripe.paymentIntents.create({
@@ -18,7 +19,93 @@ exports.processPayment = catchAsyncErrors(async (req, res, next) => {
     client_secret: paymentIntent.client_secret, // Json API client_secret
   });
 });
+const config = {
+  app_id: "2553",
+  key1: "PcY4iZIKFCIdgZvA6ueMcMHHUbRLYjPL",
+  key2: "kLtgPl8HHhfvMuDHPwKfgfsY4Ydm9eIz",
+  endpoint: "https://sb-openapi.zalopay.vn/v2/create",
+};
+exports.zaloPay = catchAsyncErrors(async (req, res) => {
+  const { totalPrice } = req.body;
+  const embed_data = {
+    redirecturl: "http://localhost:3000/success",
+  };
 
+  const items = [{}];
+  const transID = Math.floor(Math.random() * 1000000);
+  const order = {
+    app_id: config.app_id,
+    app_trans_id: `${moment().format("YYMMDD")}_${transID}`, // translation missing: vi.docs.shared.sample_code.comments.app_trans_id
+    app_user: "user123",
+    app_time: Date.now(),
+    item: JSON.stringify(items),
+    embed_data: JSON.stringify(embed_data),
+    amount: totalPrice,
+    description: `Lazada - Payment for the order #${transID}`,
+    bank_code: "",
+    callback:
+      "https://de06-2402-800-6105-34ae-48d3-3295-9365-c2fe.ngrok-free.app/api/v1/callbackzalo",
+    // bank_code: "zalopayapp",
+  };
+
+  // appid|app_trans_id|appuser|amount|apptime|embeddata|item
+  const data =
+    config.app_id +
+    "|" +
+    order.app_trans_id +
+    "|" +
+    order.app_user +
+    "|" +
+    order.amount +
+    "|" +
+    order.app_time +
+    "|" +
+    order.embed_data +
+    "|" +
+    order.item;
+  order.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
+  try {
+    const result = await axios.post(config.endpoint, null, { params: order });
+    return res.status(200).json(result.data);
+  } catch (error) {
+    console.log("error: ", error);
+  }
+});
+exports.callback = catchAsyncErrors(async (req, res) => {
+  let result = {};
+
+  try {
+    let dataStr = req.body.data;
+    let reqMac = req.body.mac;
+
+    let mac = CryptoJS.HmacSHA256(dataStr, config.key2).toString();
+    console.log("mac =", mac);
+
+    // kiểm tra callback hợp lệ (đến từ ZaloPay server)
+    if (reqMac !== mac) {
+      // callback không hợp lệ
+      result.return_code = -1;
+      result.return_message = "mac not equal";
+    } else {
+      // thanh toán thành công
+      // merchant cập nhật trạng thái cho đơn hàng
+      let dataJson = JSON.parse(dataStr, config.key2);
+      console.log(
+        "update order's status = success where app_trans_id =",
+        dataJson["app_trans_id"]
+      );
+
+      result.return_code = 1;
+      result.return_message = "success";
+    }
+  } catch (ex) {
+    result.return_code = 0; // ZaloPay server sẽ callback lại (tối đa 3 lần)
+    result.return_message = ex.message;
+  }
+
+  // thông báo kết quả cho ZaloPay server
+  res.json(result);
+});
 // Send stripe API Key   =>   /api/v1/stripeapi
 exports.sendStripApi = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
